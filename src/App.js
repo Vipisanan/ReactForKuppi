496ab9d749a72ad6cc89f6db8f2f0733734b4df5
vipisanan <vipisanan1@gmail.com>
2019-11-23 12:47:52 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 function App() {
     return (
-        <div>
-            <NavBar></NavBar>
-            <Home></Home>
-        </div>
+        <Router>
+            <div>
+                <NavBar></NavBar>
+                <hr />
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/contact' component={Contact} />
+                    <Route path='/about' component={About} />
+                </Switch>
+            </div>
+        </Router>
+
+        //
+        // <div>
+        //     <NavBar></NavBar>
+        //     <Home></Home>
+        // </div>
     );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 class NavBar extends React.Component {
     render() {
@@ -12,19 +13,9 @@ class NavBar extends React.Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabIndex="-1"
-                                   aria-disabled="true">Disabled</a>
-                            </li>
+                            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+                            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+                            <li><Link to={'/about'} className="nav-link">About</Link></li>
                         </ul>
                     </div>
                 </nav>
